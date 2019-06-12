cd45778f943dac77e9d3e79c34b31f41aba5277a
holyspice <51357134+holyspice@users.noreply.github.com>
2019-06-12 14:31:26 +0530

    Update variant-picker.js

--- saleor/static/js/components/variant-picker.js
+++ saleor/static/js/components/variant-picker.js
@@ -11,23 +11,9 @@ import {onAddToCheckoutSuccess, onAddToCheckoutError} from './checkout';
 export default $(document).ready((e) => {
   const variantPickerContainer = document.getElementById('variant-picker');
   const variantPriceContainer = document.getElementById('variant-price-component');
-  function im () {
-    var img = document.getElementById('showcase');
-    var source = img.getAttribute('src');
-    return source;
-  };
-  async function backchange () {
-    let v = new Vibrant(await im());
-    v.getPalette((err, palette) => {
-      console.log(palette.Vibrant.hex, err);
-      document.body.style.backgroundColor = palette.Muted.hex;
-      document.getElementById('navbar').style.backgroundColor = palette.Muted.hex;
-      document.getElementById('sidebar').style.backgroundColor = palette.Muted.hex;
-      document.getElementById('checkbar').style.backgroundColor = palette.Muted.hex;
-    });
-  };
-  backchange();
-
+  var img = document.getElementById('showcase');
+  var source = img.getAttribute('src');
+  console.log(source);
   if (variantPickerContainer) {
     const variantPickerData = JSON.parse(variantPickerContainer.dataset.variantPickerData);
     ReactDOM.render(
