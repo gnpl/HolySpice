075206f8dc18e7b9dce6df0ede682b5fb477c6f1
holyspice <51357134+holyspice@users.noreply.github.com>
2019-06-18 13:45:23 +0530

    Update variant-picker.js

--- saleor/static/js/components/variant-picker.js
+++ saleor/static/js/components/variant-picker.js
@@ -11,14 +11,40 @@ import {onAddToCheckoutSuccess, onAddToCheckoutError} from './checkout';
 export default $(document).ready((e) => {
   const variantPickerContainer = document.getElementById('variant-picker');
   const variantPriceContainer = document.getElementById('variant-price-component');
-  var img = document.getElementById('showcase');
-  img.setAttribute('crossOrigin', 'anonymous');
+  function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
+  function getColor() {
+  return (
+    "#" +
+    Math.random()
+      .toString(16)
+      .slice(2, 8)
+  );
+}
   img.addEventListener('load', function() {
-    Vibrant.from(img).getPalette().then(function(palette) {console.log(palette)});
-    document.body.style.backgroundColor = '#eab441';
-    document.getElementById('navbar').style.backgroundColor = '#eab441';
-    document.getElementById('sidebar').style.backgroundColor = '#e8ae30';
-    document.getElementById('checkbar').style.backgroundColor = '#eab441';
+    var col = getColor();
+    document.body.style.backgroundColor = col;
+    document.getElementById('navbar').style.backgroundColor = col;
+    document.getElementById('sidebar').style.backgroundColor = shadeColor(col,-20);
+    document.getElementById('checkbar').style.backgroundColor = col;
   });
   if (variantPickerContainer) {
     const variantPickerData = JSON.parse(variantPickerContainer.dataset.variantPickerData);
