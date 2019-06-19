6f9da2949f0ed8de2dccc477e3e76b516673f62e
holyspice <51357134+holyspice@users.noreply.github.com>
2019-06-19 11:43:12 +0530

    Update variant-picker.js

--- saleor/static/js/components/variant-picker.js
+++ saleor/static/js/components/variant-picker.js
@@ -31,20 +31,16 @@ export default $(document).ready((e) => {
 
     return "#"+RR+GG+BB;
 }
-  var items=['#73a9ad','#edad40'];
+  var items = ['#73a9ad','#edad40'];
   var img = document.getElementById('showcase');
   img.addEventListener('load', function() {
-    var cus= document.getElementById('cuisine').innerHTML;
-    var col='#f18b58';
-    if(cus == 'Indian')
-    {
-      col=items[1];
+    var cus = document.getElementById('cuisine').innerHTML;
+    var col = '#f18b58';
+    if (cus === 'Indian') {
+      col = items[1];
+    } else if (cus === 'Italian') {
+      col = items[0];
     }
-    else if(cus == 'Italian')
-    {
-      col=items[0];
-    }
-      
     document.body.style.backgroundColor = col;
     document.getElementById('navbar').style.backgroundColor = col;
     document.getElementById('sidebar').style.backgroundColor = shadeColor(col,-20);
